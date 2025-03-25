c059797f692305c5bb2d020480946f64ff6dff50
Shreyash Reshu <shreyashreshu6@gmail.com>
2025-03-26 02:16:58 +0530

    Initial commit

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,11 +1,16 @@
-const Header = () => {
+const Header = ({title}) => {
   
   
   return (
     <header>
-        <h1>Groceries List</h1>
+        <h1>{title}</h1>
     </header>
   )
 }
 
-export default Header
+Header.defaultProps={
+  title: "Default Title"
+}
+
+
+export default Header;
